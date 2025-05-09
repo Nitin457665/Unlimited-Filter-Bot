4126a4578d1913b087c8af2cda0a58a28dcfb83e
Nitin457665 <netrapaal422@gmail.com>
2025-05-09 22:51:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = 5949483204:AAGOAZp5wj3wUzyDKWyZIqkHCb8YomuO5NE
+    BOT_TOKEN = "5949483204:AAGOAZp5wj3wUzyDKWyZIqkHCb8YomuO5NE"
     API_ID = 27244266
-    API_HASH = faf5bc1ca1d7eea574fdd4b310476c7a
+    API_HASH = "faf5bc1ca1d7eea574fdd4b310476c7a"
     AUTH_USERS = [5301956231]  # yahan apna Telegram ID daal
