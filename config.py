a527a056e9a1bd29d67a1bddeaf1951dc3e8ec8e
Nitin457665 <netrapaal422@gmail.com>
2025-05-09 15:51:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-import os
-
 class Config:
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    MONGO_URI = os.environ.get("MONGO_URI")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "filterdb")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
+    BOT_TOKEN = 5949483204:AAGOAZp5wj3wUzyDKWyZIqkHCb8YomuO5NE
+    API_ID = 27244266
+    API_HASH = faf5bc1ca1d7eea574fdd4b310476c7a
+    AUTH_USERS = [5301956231]  # yahan apna Telegram ID daal
