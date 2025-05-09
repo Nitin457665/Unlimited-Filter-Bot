553320402fda022b6005014f7684db99a87d58d2
Nitin457665 <netrapaal422@gmail.com>
2025-05-09 14:48:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("Config.TG_BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
     MONGO_URI = os.environ.get("MONGO_URI")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "filterdb")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
