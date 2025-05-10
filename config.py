95538b8265d83b26e8bbad9d407294971241628c
Nitin457665 <netrapaal422@gmail.com>
2025-05-10 06:34:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config:
     API_ID = 27244266
     API_HASH = "faf5bc1ca1d7eea574fdd4b310476c7a"
     AUTH_USERS = [5301956231]  # yahan apna Telegram ID daal
+    DATABASE_URI = "mongodb+srv://cinemashor0:RYRUL115JCOM47R4@myfilterbot.cdxakym.mongodb.net/?retryWrites=true&w=majority&appName=MyFilterBot"
